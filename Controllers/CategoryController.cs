4c44e2ec3bd31ca30a0ba3ec9473d596cd97ad00
herringvoices <nalherring@gmail.com>
2025-02-11 11:41:22 -0600

    Added controller for getting category by id and posting category

--- Controllers/CategoryController.cs
+++ Controllers/CategoryController.cs
@@ -17,11 +17,38 @@ public class CategoryController : ControllerBase
         _context = context;
     }
 
+    //GET all categories
     [HttpGet]
     [Authorize(Roles = "Admin")]
     public IActionResult Get()
     {
         var categories = _context.Categories.OrderBy(c => c.Name);
         return Ok(categories.Select(c => new CategoryDTO { Id = c.Id, Name = c.Name }));
+    }
+
+    //GET a single category
+    [HttpGet("{id}")]
+    [Authorize(Roles = "Admin")]
+    public IActionResult GetById(int id)
+    {
+        var category = _context.Categories.SingleOrDefault(c => c.Id == id);
+        if (category == null)
+        {
+            return NotFound();
+        }
+        var categoryDTO = new CategoryDTO { Id = category.Id, Name = category.Name };
+        return Ok(categoryDTO);
+    }
+
+    //POST a new category
+    [HttpPost]
+    [Authorize(Roles = "Admin")]
+    public IActionResult Post([FromBody] CategoryDTO categoryDTO)
+    {
+        var category = new Category { Name = categoryDTO.Name };
+
+        _context.Categories.Add(category);
+        _context.SaveChanges();
+        return CreatedAtAction(nameof(GetById), new { id = category.Id }, categoryDTO);
     }
 }
