d7d907737467ae135fd3fa2e0db55f82fbd0c6fe
herringvoices <nalherring@gmail.com>
2025-02-11 10:23:13 -0600

    Added controller for getting all categories in alphabetical order

--- Controllers/CategoryController.cs
+++ Controllers/CategoryController.cs
@@ -1,8 +1,8 @@
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using Tabloid.Data;
-using Tabloid.Models;
 using Tabloid.DTOs;
+using Tabloid.Models;
 
 namespace Tabloid.Controllers;
 
@@ -17,4 +17,11 @@ public class CategoryController : ControllerBase
         _context = context;
     }
 
+    [HttpGet]
+    [Authorize(Roles = "Admin")]
+    public IActionResult Get()
+    {
+        var categories = _context.Categories.OrderBy(c => c.Name);
+        return Ok(categories.Select(c => new CategoryDTO { Id = c.Id, Name = c.Name }));
+    }
 }
