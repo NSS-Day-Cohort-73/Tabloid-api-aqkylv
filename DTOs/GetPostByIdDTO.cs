f83cc7f1117f1a516a6f5c3f682d93b5cb1aab6b
herringvoices <nalherring@gmail.com>
2025-02-19 15:11:47 -0600

    Added query param searching to get all posts endpoint. Updated Post model for a reason I've forgotten.

--- DTOs/GetPostByIdDTO.cs
+++ DTOs/GetPostByIdDTO.cs
@@ -12,12 +12,11 @@ public class GetPostByIdDTO
     public UserProfileForPostByIdDTO Author { get; set; }
 
     public string Title { get; set; }
+    public string SubTitle { get; set; }
 
     public DateTime PublishingDate { get; set; }
 
     public string HeaderImage { get; set; } = "";
 
     public string Content { get; set; }
-
-
 }
